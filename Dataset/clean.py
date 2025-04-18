8e7b412d0c0e71e18b1910e3037b043124d45306
sakanaowo <idiotbugg@gmail.com>
2025-04-18 09:36:20 +0700

    update

--- Dataset/clean.py
+++ Dataset/clean.py
@@ -1,25 +1,13 @@
 import json
-import os
 
-# Đường dẫn đến file labels.json gốc
-original_json_path = 'labels.json'
-
-# Đọc dữ liệu gốc
-with open(original_json_path, 'r') as f:
+with open("cleaned_labels.json", 'r', encoding="utf-8") as f:
     data = json.load(f)
-
-# Tạo dictionary mới với đường dẫn tương đối
-cleaned_data = {}
-
-for abs_path, label in data.items():
-    # Lấy phần đường dẫn bắt đầu từ 'dataset'
-    rel_path = os.path.relpath(abs_path, start="D:\\Dragon Fruit project (CNN)\\")
-    rel_path = rel_path.replace("\\", "/").replace("/D:/Dragon Fruit project (CNN)","")
-    cleaned_data[rel_path] = label
-
-# Ghi ra file mới
-output_path = 'cleaned_labels.json'
-with open(output_path, 'w') as f:
-    json.dump(cleaned_data, f, indent=2)
-
-print(f"✅ Done! Cleaned labels saved to: {output_path}")
+i = 1
+processed_data = {}
+for path, label in data.items():
+    new_path = path.replace("..", "")
+    processed_data[new_path] = label
+    print(i,new_path)
+    i += 1
+with open("cleaned_labels.json", 'w', encoding="utf-8") as f:
+    json.dump(processed_data, f, indent=4, ensure_ascii=False)
