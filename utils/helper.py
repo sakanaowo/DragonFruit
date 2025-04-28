1c3a4d5786a10513573bb5b3f254f372a71ff0fa
sakanaowo <idiotbugg@gmail.com>
2025-04-28 20:58:56 +0700

    update: save model

---
+++ utils/helper.py
@@ -0,0 +1,10 @@
+import torch
+import os
+
+
+def save_model(model, path):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    torch.save(model.state_dict(), path)
+    print('model saved')
+
+# def load_model(model, path):
