1a3f40b40e899db40a16c79741508b88ec278837
sakanaowo <idiotbugg@gmail.com>
2025-05-01 20:48:27 +0700

    add weight calc

--- src/dataset.py
+++ src/dataset.py
@@ -1,5 +1,6 @@
+import numpy as np
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler
 from utils.transform import train_transform, val_test_transform
 
 
@@ -8,9 +9,20 @@ def get_dataloaders(train_dir, val_dir, test_dir, batch_size, num_workers):
     val_dataset = datasets.ImageFolder(val_dir, transform=val_test_transform)
     test_dataset = datasets.ImageFolder(test_dir, transform=val_test_transform)
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=num_workers)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers)
+    targets = [sample[1] for sample in train_dataset.samples]
+    class_counts = np.bincount(targets)
+    class_weights = 1. / class_counts
+    sample_weights = [class_weights[t] for t in targets]
+    sampler = WeightedRandomSampler(sample_weights, num_samples=len(sample_weights), replacement=True)
+
+    # train_loader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers)
+    # val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=num_workers)
+    # test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, sampler=sampler, num_workers=num_workers)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
 
     dataloaders = {
         'train': train_loader,
