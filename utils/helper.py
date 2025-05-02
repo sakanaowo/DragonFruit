3e39dd2f1a25f8cd6daf4151a6241edd48e66dc7
sakanaowo <idiotbugg@gmail.com>
2025-05-02 18:29:53 +0700

    Add training logs for first model training session
    This commit includes detailed logs for the first training run of the model over 50 epochs. Key metrics such as training/validation loss and accuracy are recorded, and checkpoints are saved at significant milestones.

--- utils/helper.py
+++ utils/helper.py
@@ -1,5 +1,11 @@
 import torch
 import os
+import re
+from utils.config import load_config
+
+from matplotlib import pyplot as plt
+
+epochs = list(range(1, 51))
 
 
 def save_model(model, path):
@@ -7,4 +13,59 @@ def save_model(model, path):
     torch.save(model.state_dict(), path)
     print('model saved')
 
+
 # def load_model(model, path):
+def load_logs(file_name):
+    log_dir = load_config()['output']['log_dir']
+    with open(log_dir + "/" + file_name, 'r') as f:
+        return f.read()
+
+
+def extract_metrics_from_log(log_text):
+    train_loss, val_loss = [], []
+    train_acc, val_acc = [], []
+
+    # Tìm tất cả các dòng chứa thông số
+    lines = log_text.strip().split('\n')
+    for line in lines:
+        match = re.match(r"Train Loss: ([\d.]+)\|Train Acc: ([\d.]+)%", line)
+        if match:
+            train_loss.append(float(match.group(1)))
+            train_acc.append(float(match.group(2)))
+        else:
+            match = re.match(r"Val Loss: ([\d.]+)\|Val Acc: ([\d.]+)%", line)
+            if match:
+                val_loss.append(float(match.group(1)))
+                val_acc.append(float(match.group(2)))
+
+    return [train_loss, val_loss, train_acc, val_acc]
+
+
+def show_loss(train_loss, val_loss):
+    plt.figure(figsize=(12, 5))
+    # plt.subplot(1, 2, 1)
+    plt.plot(epochs, train_loss, label='Train Loss', marker='o')
+    plt.plot(epochs, val_loss, label='Val Loss', marker='o')
+    plt.title('Loss per Epoch')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.show()
+
+
+def show_acc(train_acc, val_acc):
+    plt.figure(figsize=(12, 5))
+    # plt.subplot(1, 2, 2)
+    plt.plot(epochs, train_acc, label='Train Acc', marker='o')
+    plt.plot(epochs, val_acc, label='Val Acc', marker='o')
+    plt.title('Accuracy per Epoch')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy (%)')
+    plt.legend()
+    plt.grid(True)
+
+    plt.tight_layout()
+    plt.show()
