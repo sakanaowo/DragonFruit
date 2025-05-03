030c277b7b59b5fa9e50ed5847f581b1bea7ded5
sakanaowo <idiotbugg@gmail.com>
2025-05-03 16:24:19 +0700

    Add evaluation script for model performance metrics
    This script introduces an `evaluate` function to assess model accuracy and classification performance using a test dataset. It includes accuracy computation, a classification report, and integrates seamlessly with the existing configuration and dataloader setup.

---
+++ src/evaluate.py
@@ -0,0 +1,48 @@
+import torch
+from torch.utils.data import DataLoader
+from sklearn.metrics import classification_report, accuracy_score
+from tqdm import tqdm
+
+from src.dataset import get_dataloaders
+from utils.config import load_config
+from utils.load_model import load_model
+
+
+def evaluate(model, dataloader, device, class_name):
+    all_preds = []
+    all_labels = []
+
+    with torch.no_grad():
+        for images, labels in tqdm(dataloader, desc='Evaluating'):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            _, preds = torch.max(outputs, 1)
+
+            all_preds.extend(preds.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+
+    acc = accuracy_score(all_labels, all_preds)
+    report = classification_report(all_labels, all_preds, target_names=class_name)
+
+    print(f"Accuracy: {acc * 100:.2f}%")
+    print("Classification Report:", report)
+
+    return acc, report
+
+
+if __name__ == '__main__':
+    config = load_config()
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = load_model(device)
+
+    dataloader = get_dataloaders(
+        config['data']['train_dir'],
+        config['data']['val_dir'],
+        config['data']['test_dir'],
+        config['data']['batch_size'],
+        config['data']['num_workers'],
+    )
+    classes_names = dataloader['train'].dataset.classes
+    evaluate(model, dataloader['test'], device, classes_names)
