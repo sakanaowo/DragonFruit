3d26df1074d3cd0063627e40080f201bb2ec0ae2
sakanaowo <idiotbugg@gmail.com>
2025-05-05 18:44:07 +0700

    Refactor transforms and enhance data augmentation
    Extract mean/std normalization values into variables for reuse. Added vertical flip and color jitter to training augmentations to improve model robustness. Commented out unused random resized crop transformation.

--- utils/transform.py
+++ utils/transform.py
@@ -2,18 +2,21 @@
 
 from torchvision import transforms
 
+mean = [0.485, 0.456, 0.406],
+std = [0.229, 0.224, 0.225]
 train_transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomRotation(degrees=15),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+    # transforms.RandomResizedCrop(224, scale=(0.8, 1.0)),
+    transforms.RandomVerticalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=mean, std=std)
 ])
 
 val_test_transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225])
+    transforms.Normalize(mean=mean, std=std)
 ])
