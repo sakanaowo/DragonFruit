7211b3d8a562cce21fccfe099853b143b37f8129
sakanaowo <idiotbugg@gmail.com>
2025-05-05 18:52:25 +0700

    Add ConvertToRGB transformation to train pipeline
    Introduced a ConvertToRGB class to ensure input images are converted to RGB format. This step is now included in the training transformation pipeline to standardize image format and prevent potential compatibility issues.

--- utils/transform.py
+++ utils/transform.py
@@ -4,8 +4,14 @@ from torchvision import transforms
 
 mean = [0.485, 0.456, 0.406],
 std = [0.229, 0.224, 0.225]
+
+class ConvertToRGB:
+    def __call__(self, img):
+        return img.convert("RGB")
+
 train_transform = transforms.Compose([
     transforms.Resize((224, 224)),
+    ConvertToRGB(),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomRotation(degrees=15),
     transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
